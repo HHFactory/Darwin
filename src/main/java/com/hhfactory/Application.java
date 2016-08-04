074001739c4516d938e83c68961bc0cf82a07bf7
saku <saku@saku-no-MacBook-Pro.local>
2016-08-04 21:09:06 +0900

    Revert "modify とりま修正しました。"
    This reverts commit d4d6f3f44e1dec31d2f58baf630a57ead4046004.

--- src/main/java/com/hhfactory/Application.java
+++ src/main/java/com/hhfactory/Application.java
@@ -3,11 +3,9 @@ package com.hhfactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy(proxyTargetClass = true)
-@PropertySource({"classpath:application.properties"})
 public class Application {
 
 	public static void main(String[] args) {
