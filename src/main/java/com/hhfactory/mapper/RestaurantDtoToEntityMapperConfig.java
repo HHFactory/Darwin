6f1a8a92c22891e3318da18fb8dcd251af4c2be6
HHFactory <hidenori.horinouchi.vxc@gmail.com>
2016-08-07 19:15:47 +0900

    add レストランオブジェクトマッパーを定義

---
+++ src/main/java/com/hhfactory/mapper/RestaurantDtoToEntityMapperConfig.java
@@ -0,0 +1,12 @@
+package com.hhfactory.mapper;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * RestaurantDtoからRestaurantEntityへのマッピング定義クラス
+ *
+ */
+@Configuration
+public class RestaurantDtoToEntityMapperConfig {
+	
+}
