70ce132abd09c19a181cc3a683b0b94eee080cca
HHFactory <hidenori.horinouchi.vxc@gmail.com>
2016-08-16 00:03:30 +0900

    modify mapperクラス名の変更

--- src/main/java/com/hhfactory/mapper/ByteConverter.java
+++ src/main/java/com/hhfactory/mapper/ByteConverter.java
@@ -22,31 +22,27 @@ public class ByteConverter {
 
 	/**
 	 * LatLng型をGeometory型のbyte配列に変換する
-	 * @param attribute
-	 * @return
+	 * @param targetLocation[LatLng]:経度緯度情報
+	 * @return Mysql Geometory型byte配列
 	 */
-    public static byte[] convertToGeometoryData(LatLng attribute) {
-        if (attribute == null) {
-            return null;        	
-        }
-
+    public static byte[] convertToGeometoryData(LatLng targetLocation) {
     	// Geometry型byte配列として25バイトのメモリを確保し、Geometryの頭9バイトを格納しておく
         ByteBuffer resultBuffer = ByteBuffer.allocate(25).put(PREFIX_BYTES);
         // double型の経度・緯度のbyte配列として8バイトのメモリを確保
         ByteBuffer doubleBuffer = ByteBuffer.allocate(8);
         // 緯度をdouble型に変換 -> byte配列に変換 -> Geometry型byte配列に格納（XXX:緯度から先に格納すること）
-        putReversedData(resultBuffer, doubleBuffer, attribute.getLat().floatValue());
+        putReversedData(resultBuffer, doubleBuffer, targetLocation.getLat().floatValue());
         // 経度をdouble型に変換 -> byte配列に変換 -> Geometry型byte配列に格納
-        putReversedData(resultBuffer, doubleBuffer, attribute.getLng().floatValue());
+        putReversedData(resultBuffer, doubleBuffer, targetLocation.getLng().floatValue());
 
         return resultBuffer.array();
     }
 
     /**
      * 引数で渡された経度・緯度をMYSQLのGeometry型のbyte配列に変換する
-     * @param target
-     * @param buffer
-     * @param value
+     * @param geometroyByteBuffer[ByteBuffer]:geometory型byteデータを格納するbytebuffer
+     * @param doubleByteBuffer[ByteBuffer]:経度緯度を格納するbytebuffer
+     * @param value[double]:double型に変換した経度緯度データ
      */
     private static void putReversedData(ByteBuffer geometoryByteBuffer, ByteBuffer doubleByteBuffer, double value) {
         // doubleByteに引数の経度・緯度のデータを格納し、反転させてgeometoryByteに格納
@@ -59,10 +55,10 @@ public class ByteConverter {
     
 	/**
 	 * 指定された位置から7バイト分の配列を反転させて、double型にして返す
-	 * @param buffer
-	 * @param data
-	 * @param position
-	 * @return 
+	 * @param buffer[ByteBuffer]:経度緯度のbytebuffer
+	 * @param data[byte]:byte配列に変換した経度緯度情報
+	 * @param position:反転開始位置
+	 * @return byte配列反転後のdouble型データ
 	 */
 	public static double getReversedData(ByteBuffer buffer, byte[] data, int position) {
         buffer.position(0);
