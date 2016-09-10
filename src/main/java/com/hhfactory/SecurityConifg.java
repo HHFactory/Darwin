2d16295b91a80fcb3a46021876a9c3f9d13d6404
saku <saku@saku-no-MacBook-Pro.local>
2016-09-10 21:59:05 +0900

    modify SecurityConfig.java にもらった本に書かれてた部分を追加

--- src/main/java/com/hhfactory/SecurityConifg.java
+++ src/main/java/com/hhfactory/SecurityConifg.java
@@ -1,10 +1,30 @@
 package com.hhfactory;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.encoding.PasswordEncoder;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebMvcSecurity
 public class SecurityConifg extends WebSecurityConfigurerAdapter {
-	//ここにもらったSpringのセキュリティ部分を記載する
+	//ここにもらったSpring本のセキュリティ部分を記載する
+	@Configuration
+	static class AuthenticationConfiguration extends GlobalAuthenticationConfigurerAdapter{
+		@Autowired
+		UserDatailsService userDetailsService;
+		@Bean
+		PasswordEncoder passwordEncoder() {
+			return new BCryptPasswordEncoder();
+		}
+		@Override
+		public void init(AuthenticationManagerBuilder auth) throws Exception{
+			auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+		}
+		
+	}
 }
