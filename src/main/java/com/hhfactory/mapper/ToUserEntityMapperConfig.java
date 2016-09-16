317266151ce83196bba4390582afa19846051aec
saku <saku@saku-no-MacBook-Pro.local>
2016-09-17 00:49:51 +0900

    modify ToUserENtityMapperConfig

--- src/main/java/com/hhfactory/mapper/ToUserEntityMapperConfig.java
+++ src/main/java/com/hhfactory/mapper/ToUserEntityMapperConfig.java
@@ -1,9 +1,29 @@
 package com.hhfactory.mapper;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.io.IOException;
 
-import com.hhfactory.service.implement.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.hhfactory.dto.UserDto;
+import com.hhfactory.entity.UserEntity;
 
 public class ToUserEntityMapperConfig {
-	//ここにパスワードをハッシュ化するメソッドを書く
+	@Autowired
+	private UserDto userDto;
+	@Autowired
+	private UserEntity userEntity;
+	
+	PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+	
+	/**
+	 * UserDtoからパスワードをハッシュ化する
+	 * @param address[String]:住所
+	 * @return ハッシュ化した数値
+	 * @throws IOException 
+	 */
+	public String passwordencode(String password){
+		return passwordEncoder.encode(password);	
+	}
 }
